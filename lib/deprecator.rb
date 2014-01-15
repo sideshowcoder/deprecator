0ca3ec7065da5ef307c27240d9d043c3677abafe
Philipp Fehre <philipp.fehre@googlemail.com>
2014-01-15 09:51:44 +0100

    implemented exact version matching

--- lib/deprecator.rb
+++ lib/deprecator.rb
@@ -20,6 +20,12 @@ module Deprecator
           callbacks.each { |cb| run_callback cb, expected_version }
         end
       end
+
+      self.class.__matched.each do |expected_version, callbacks|
+        if version != expected_version
+          callbacks.each { |cb| run_callback cb, expected_version }
+        end
+      end
     end
 
     private
@@ -33,12 +39,30 @@ module Deprecator
     end
 
     module ClassMethods
-      attr_reader :__ensured
-
       def ensure_version expected_version, upgrade_method = nil, &block
-        @__ensured ||= Hash.new { |hash, key| hash[key] = [] }
-        @__ensured[expected_version] << block if block_given?
-        @__ensured[expected_version] << upgrade_method if upgrade_method
+        __ensured[expected_version] << block if block_given?
+        __ensured[expected_version] << upgrade_method if upgrade_method
+      end
+
+      def match_version expected_version, missmatch_method = nil, &block
+        __matched[expected_version] << block if block_given?
+        __matched[expected_version] << missmatch_method if missmatch_method
+      end
+
+      # Internal handling for storing the hook callbacks
+      def __versions
+        @__versions ||= {
+          ensured: Hash.new { |hash, key| hash[key] = [] },
+          matched: Hash.new { |hash, key| hash[key] = [] }
+        }
+      end
+
+      def __ensured
+        __versions[:ensured]
+      end
+
+      def __matched
+        __versions[:matched]
       end
     end
   end
--- test/deprecator_test.rb
+++ test/deprecator_test.rb
@@ -1,9 +1,8 @@
 require "test_helper"
 
-describe "ensureing versions" do
-
-  before do
-    @subject = Class.new do
+describe Deprecator::Versioning do
+  subject do
+    Class.new do
       attr_reader :version
 
       def initialize args
@@ -14,41 +13,62 @@ describe "ensureing versions" do
     end
   end
 
-  it "does nothing if the version matches" do
-    @subject.instance_eval do
-      ensure_version 2, :version_upgrade
-      define_method(:version_upgrade) do |_|
-        raise "version_upgrade should not be called"
+  describe "#ensure_version" do
+    it "does nothing if the version matches" do
+      subject.instance_eval do
+        ensure_version 2, :version_upgrade
+        define_method(:version_upgrade) do |_|
+          raise "version_upgrade should not be called"
+        end
       end
+      subject.new(version: 2)
     end
-    @subject.new(version: 2)
-  end
 
-  it "calls the upgrade_method on version mismatch" do
-    @subject.instance_eval do
-      ensure_version 2, :version_upgrade
-      define_method(:version_upgrade) do |new_version|
-        @version = new_version
+    it "calls the upgrade_method on version mismatch" do
+      subject.instance_eval do
+        ensure_version 2, :version_upgrade
+        define_method(:version_upgrade) do |new_version|
+          @version = new_version
+        end
       end
+
+      subject.new(version: 1).version.must_equal 2
     end
 
-    @subject.new(version: 1).version.must_equal 2
-  end
+    it "allows a lambda to be passed as the upgrade handler" do
+      subject.instance_eval do
+        ensure_version 2, ->(new_version) { @version = new_version }
+      end
 
-  it "allows a lambda to be passed as the upgrade handler" do
-    @subject.instance_eval do
-      ensure_version 2, ->(new_version) { @version = new_version }
+      subject.new(version: 1).version.must_equal 2
     end
 
-    @subject.new(version: 1).version.must_equal 2
-  end
+    it "allows a block to be passed as the upgrade handler" do
+      subject.instance_eval do
+        ensure_version(2) { |expected_version| @version = expected_version }
+      end
 
-  it "allows a block to be passed as the upgrade handler" do
-    @subject.instance_eval do
-      ensure_version(2) { |expected_version| @version = expected_version }
+      subject.new(version: 1).version.must_equal 2
     end
 
-    @subject.new(version: 1).version.must_equal 2
   end
 
+  describe "#match_version" do
+    it "does nothting if the version matches" do
+      subject.instance_eval do
+        match_version 2, :missmatch
+        define_method(:missmatch) { raise "should not call missmatch" }
+      end
+      subject.new(version: 2)
+    end
+
+    it "call the missmatch handler if version is does not match" do
+      subject.instance_eval do
+        match_version 2, :missmatch
+        define_method(:missmatch) { |expected_version| @version = expected_version }
+      end
+      subject.new(version: 1).version.must_equal 2
+      subject.new(version: 3).version.must_equal 2
+    end
+  end
 end
